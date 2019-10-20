7c86d944673f3728d21e11486c7da7d63f304357
Thom Wiggers <thom@thomwiggers.nl>
2019-10-20 16:22:32 +0200

    Get rid of BrandEAN intermediate

--- interface/forms.py
+++ interface/forms.py
@@ -9,16 +9,6 @@ class ProductScannerForm(forms.Form):
     ean = EANFormField()
 
 
-class BrandEANAddForm(forms.ModelForm):
-    class Meta:
-        model = models.BrandEAN
-        fields = ['label', 'brand']
-        widgets = {
-            'brand': autocomplete.ModelSelect2(
-                url='interface:brand-autocomplete'),
-        }
-
-
 class PackagingForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -33,18 +23,23 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = models.Product
         fields = '__all__'
+        widgets = {
+            'brand': autocomplete.ModelSelect2(
+                url='interface:brand-autocomplete',
+            ),
+        }
 
 
 class SelectProductForm(forms.Form):
     def __init__(self, *args, brand, **kwargs):
         super().__init__(*args, **kwargs)
-        self.brand = brand
-        self.fields['product'].queryset = brand.product_set.all()
-        self.fields['product'].widget.forward = [
-            forward.Const(brand.pk, 'brand')]
+        if brand is not None:
+            self.fields['product'].queryset = brand.product_set.all()
+            self.fields['product'].widget.forward = [
+                forward.Const(brand.pk, 'brand')]
 
     product = forms.ModelChoiceField(
-        queryset=models.Brand.objects.all(),
+        queryset=models.Product.objects.all(),
         widget=autocomplete.ModelSelect2(
             url='interface:product-autocomplete',
         ),
